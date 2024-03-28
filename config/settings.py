27a6cc3fa51f98fed151048fa1ade3b9fbd98401
mimimichaello <137781824+mimimichaello@users.noreply.github.com>
2024-03-28 20:15:37 +0300

    Created the follower, models, serializers, ListFollowerView application

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "profiles",
     "wall",
     "comments",
+    "followers",
     "rest_framework",
     "djoser",
     "drf_yasg",
---
+++ followers/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+
+class Follower(models.Model):
+    """ Follower model
+    """
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
